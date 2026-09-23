8a80eb080a6d5351cfb4942bcb8c567f01fb0e09
dmogire <dnm40@bath.ac.uk>
2026-09-23 14:36:59 +0100

    L6: new view

---
+++ real-world-vue/src/views/EventDetailsView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+
+const event = ref(null)
+
+onMounted(() => {
+  // fetch event by id and set event to response data
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
